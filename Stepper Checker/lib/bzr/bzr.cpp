3727d46f5ad4ec3ecf3c4adff5b78bf5ef4b7738
ahmad.faris <ahmadfarisfs@gmail.com>
2022-05-28 10:44:36 +0700

    add buzzer class

---
+++ Stepper Checker/lib/bzr/bzr.cpp
@@ -0,0 +1,22 @@
+#include "bzr.h"
+
+Buzzer::Buzzer(uint8_t pin){
+    m_pin=pin;
+}
+
+void Buzzer::buzz(uint16_t duration){
+    //Do not interrupt ongoing beep
+    if (digitalRead(m_pin))
+    {
+        return;
+    }
+    digitalWrite(m_pin, HIGH);
+    m_buzzerEndTime = millis() + duration;
+}
+
+void Buzzer::loop(){
+    if (millis() > m_buzzerEndTime)
+    {
+        digitalWrite(m_pin, LOW);
+    }
+}
---
+++ Stepper Checker/lib/bzr/bzr.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <Arduino.h>
+class Buzzer{
+  public:
+    Buzzer(uint8_t pin);
+    void buzz(uint16_t duration);
+    void loop();
+    private:
+    uint8_t m_pin;
+    unsigned long m_buzzerEndTime;
+};
