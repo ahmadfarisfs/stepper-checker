7853eb51c95139ea6d8e66f2a5e33fd3d297c3f0
ahmad.faris <ahmadfarisfs@gmail.com>
2022-05-28 20:03:51 +0700

    integrate blinker to main

--- Stepper Checker/src/main.cpp
+++ Stepper Checker/src/main.cpp
@@ -8,28 +8,30 @@
 #include "nonstd.h"
 #include "manager.h"
 #include "lcd.h"
-
+#include "bzr.h"
+#include "blinky.h"
 Button moveR(BTN_MOVE_R_PIN, true);
 Button moveL(BTN_MOVE_L_PIN, true);
 Button speedUp(BTN_SPEED_UP_PIN, true);
 Button speedDown(BTN_SPEED_DOWN_PIN, true);
 Button enter(BTN_EN_PIN, true);
 Motor motor(EN_PIN, STEP_PIN, DIR_PIN);
+Buzzer buzzer(BZR_PIN);
 U8G2_SSD1306_128X32_UNIVISION_1_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
 
 LCD display(&u8g2);
-StepperTesterManager manager(&moveR,&moveL,&speedDown,&speedUp,&enter,&motor,&display);
-
+StepperTesterManager manager(&moveR, &moveL, &speedDown, &speedUp, &enter, &motor, &display, &buzzer);
+Blinky status(STATUS_LED_PIN);
 void setup()
 {
   Serial.begin(115200);
   Serial.println("=BEGIN=");
-  pinMode(STATUS_LED_PIN, OUTPUT); // status led
-  pinMode(BZR_PIN, OUTPUT); // speaker
+  status.setDuration(500);
   manager.setup();
 }
 
 void loop()
 {
   manager.loop();
+  status.loop();
 }
