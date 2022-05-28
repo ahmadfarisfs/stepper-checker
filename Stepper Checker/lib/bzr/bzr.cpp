307b22f7fcc2cd55bf7362c35aaa084bdd1cb0a1
ahmad.faris <ahmadfarisfs@gmail.com>
2022-05-28 20:02:58 +0700

    add setup on buzzer lib

--- Stepper Checker/lib/bzr/bzr.cpp
+++ Stepper Checker/lib/bzr/bzr.cpp
@@ -4,6 +4,10 @@ Buzzer::Buzzer(uint8_t pin){
     m_pin=pin;
 }
 
+void Buzzer::setup(){
+    pinMode(m_pin,OUTPUT);
+}
+
 void Buzzer::buzz(uint16_t duration){
     //Do not interrupt ongoing beep
     if (digitalRead(m_pin))
--- Stepper Checker/lib/bzr/bzr.h
+++ Stepper Checker/lib/bzr/bzr.h
@@ -5,6 +5,7 @@ class Buzzer{
     Buzzer(uint8_t pin);
     void buzz(uint16_t duration);
     void loop();
+    void setup();
     private:
     uint8_t m_pin;
     unsigned long m_buzzerEndTime;
